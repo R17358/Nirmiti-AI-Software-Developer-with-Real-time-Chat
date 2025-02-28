d011d2c31fe6597fbe21dc5a41fbc91ac65b0735
R17358 <shivay.lfc@gmail.com>
2025-03-01 01:08:26 +0530

    rebuild Home.jsx chat

--- frontend-soen/src/App.jsx
+++ frontend-soen/src/App.jsx
@@ -3,17 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import AppRoutes from "./AppRoutes/AppRoutes"
-import dotenv from "dotenv"
-
-dotenv.config()
 
 function App() {
   
-  if (process.env.NODE_ENV === "production") {
-    console.log = (message) => window.__DEBUG__ && console.info(message);
-}
-window.__DEBUG__ = true;
-
   return (
     <AppRoutes />
   )
