f49911343f7d747fb3ebcd681602f7aae4a4056b
R17358 <shivay.lfc@gmail.com>
2025-02-28 14:44:37 +0530

    SOEN22

--- frontend-soen/src/api/socket.js
+++ frontend-soen/src/api/socket.js
@@ -5,7 +5,7 @@ let socketInstance = null;
 //wss://soen-ai-software-developer.onrender.com
 
 export const initializeSocket = (projectId) => {
-    socketInstance = io('wss://soen-ai-software-developer.onrender.com', {
+    socketInstance = io('ws://soen-ai-software-developer.onrender.com', {
         auth: {
         token: localStorage.getItem("token")
         },
