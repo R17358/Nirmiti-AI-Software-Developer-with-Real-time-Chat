3f7cf7243f55e1c4dd3388f8ca3bf004d653be86
R17358 <shivay.lfc@gmail.com>
2025-07-08 14:08:21 +0530

    nav2

--- frontend-soen/src/Components/Header/Header.jsx
+++ frontend-soen/src/Components/Header/Header.jsx
@@ -1,28 +1,25 @@
-import React from 'react'
-import './Header.css'
-import logo from '../../assets/soen_logo.png'
-import { useState } from 'react';
-import {useSelector} from 'react-redux'
+import React, { useEffect } from 'react';
+import './Header.css';
+import logo from '../../assets/soen_logo.png';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-function Header({setCollapse, collapse}) {
+function Header({ setCollapse, collapse }) {
+  const { isAuthenticated, user } = useSelector((state) => state.user);
 
-  
-  // const [collapse, setCollapse] = useState(false);
-  const {isAuthenticated, user} = useSelector((state) => state.user);
-
-    const handleCollapse = ()=>{
-        setCollapse(!collapse);
-        const dashboardElements = document.querySelectorAll(".Dashboard");
-        dashboardElements.forEach((element) => {
-          if (collapse) {
-            document.body.classList.remove("collapse");
-          } else {
-            document.body.classList.add("collapse");
-          }
-        })
+  // Automatically add/remove body class when collapse changes
+  useEffect(() => {
+    const dashboardElements = document.querySelectorAll('.Dashboard');
+    if (collapse) {
+      document.body.classList.add('collapse');
+    } else {
+      document.body.classList.remove('collapse');
     }
-   // console.log("user",user.name)
+  }, [collapse]);
+
+  const handleCollapse = () => {
+    setCollapse(!collapse);
+  };
 
   return (
     <div className='header'>
@@ -30,26 +27,29 @@ function Header({setCollapse, collapse}) {
         <img src={logo} alt="logo" />
       </div>
       <div className="button-collapse">
-      {collapse ? (
-  <button type="button" onClick={handleCollapse}>
-    <i className="bx bx-menu" style={{ transform: 'rotate(90deg)', color: '#fff1f1' }}></i>
-  </button>
-) : (
-  <button type="button" onClick={handleCollapse}>
-    <i className="bx bx-menu" style={{ transform: 'scaleY(-1)', color: '#fff4f4' }}></i>
-  </button>
-)}   
+        <button type="button" onClick={handleCollapse}>
+          <i
+            className="bx bx-menu"
+            style={{
+              transform: collapse ? 'rotate(90deg)' : 'scaleY(-1)',
+              color: '#fff1f1',
+            }}
+          ></i>
+        </button>
 
-      {isAuthenticated?
-     ( <div className="header-username">
-        <h2>{user?.email}</h2>
-       <Link to="/logout"><i className="bx bx-log-out" style={{ color: '#ffeeee' }}></i>
-
-       </Link>
-        </div>): (<br/>)}
+        {isAuthenticated ? (
+          <div className="header-username">
+            <h2>{user?.email}</h2>
+            <Link to="/logout">
+              <i className="bx bx-log-out" style={{ color: '#ffeeee' }}></i>
+            </Link>
+          </div>
+        ) : (
+          <br />
+        )}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
